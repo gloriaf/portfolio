1435c28552239d31e9216c5925e05d2d71ff26e6
galapaga63 <magloriaff@yahoo.com>
2012-11-08 11:06:33 +0100

    Inicio rails basico

---
+++ Gemfile
@@ -0,0 +1,27 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.8'
+
+gem 'jquery-rails'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :development, :test do
+  gem 'sqlite3'
+  gem 'rspec-rails'
+end
+
+group :test do
+  gem 'capybara'
+end
+
+group :production do
+  gem 'pg' 
+end
+
+# To use debugger
+# gem 'debugger'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Portfolio::Application.config.secret_token = '3d66a7a554e67e37ab6b1c006631bcf2fc9083b255bad7d2e86f22cfd62da00d0fbff7b666ab1230484bd25eb555f3f5b4251122f8af509b849a714fd287dfb4'
